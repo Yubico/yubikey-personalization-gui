0a5cd9f9b2e3ab3d1dfa9f15ef0c6b06a9e1ec20
Klas Lindfors <klas@yubico.com>
2012-06-05 08:33:44 +0200

    bump versions post-release

--- src/version.h
+++ src/version.h
@@ -7,8 +7,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_PRODUCTVERSION          3,0,5,0
-#define VER_PRODUCTVERSION_STR      "3.0.5\0"
+#define VER_PRODUCTVERSION          3,0,6,0
+#define VER_PRODUCTVERSION_STR      "3.0.6\0"
 
 #define VER_FILEDESCRIPTION_STR     "YubiKey Personalization Tool"
 #define VER_INTERNALNAME_STR        "YKPersonalization"
