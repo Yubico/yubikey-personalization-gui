7e9dd19bf7f20917ac45dfe72181524718a241f3
Klas Lindfors <klas@yubico.com>
2012-11-12 12:09:39 +0100

    features for yubikey 3.0 (neo)

--- src/yubikeyfinder.cpp
+++ src/yubikeyfinder.cpp
@@ -44,7 +44,7 @@ const unsigned int YubiKeyFinder::FEATURE_MATRIX[][2] = {
     { YK_VERSION(2,1,7), 0 },   //Feature_MovingFactor
     { YK_VERSION(2,3,0), 0 },   //Feature_ChallengeResponseFixed
     { YK_VERSION(2,3,0), 0 },   //Feature_Updatable
-    { YK_VERSION(2,1,4), YK_VERSION(2,2,0)}, //Feature_Ndef
+    { YK_VERSION(2,1,4), 0 },   //Feature_Ndef
 };
 
 // when a featureset should be excluded from versions (NEO, I'm looking at you.)
@@ -61,7 +61,7 @@ const unsigned int YubiKeyFinder::FEATURE_MATRIX_EXCLUDE[][2] = {
     { 0, 0 },                                 //Feature_MovingFactor
     { 0, 0 },                                 //Feature_ChallengeResponseFixed
     { 0, 0 },                                 //Feature_Updatable
-    { 0, 0 },                                 //Feature_Ndef
+    { YK_VERSION(2,2,0), YK_VERSION(3,0,0) }, //Feature_Ndef
 };
 
 YubiKeyFinder::YubiKeyFinder() {
