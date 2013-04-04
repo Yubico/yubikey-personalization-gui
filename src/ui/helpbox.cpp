7926d45bc0c4b5c5b6f202b72b4cb5269dc43522
Simon Josefsson <simon@josefsson.org>
2013-04-04 15:19:31 +0200

    Bump copyright years.

--- src/ui/helpbox.cpp
+++ src/ui/helpbox.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2011-2012 Yubico AB.  All rights reserved.
+Copyright (C) 2011-2013 Yubico AB.  All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
