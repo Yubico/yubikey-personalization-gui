52c52d3ff3acbadb7c08b296c4325e376e95baeb
Klas Lindfors <klas@yubico.com>
2016-01-04 12:14:27 +0100

    update copyright years

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2011-2015 Yubico AB.  All rights reserved.
+Copyright (C) 2011-2016 Yubico AB.  All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -34,7 +34,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #define VER_FILEDESCRIPTION_STR     "YubiKey Personalization Tool"
 #define VER_INTERNALNAME_STR        "YKPersonalization"
-#define VER_LEGALCOPYRIGHT          "Copyright © 2011-2015 Yubico"
+#define VER_LEGALCOPYRIGHT          "Copyright © 2011-2016 Yubico"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_ORIGINALFILENAME_STR    "YKPersonalization.exe"
 #define VER_PRODUCTNAME_STR         "YKPersonalization"
